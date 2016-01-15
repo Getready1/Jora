fbb2bcde1e47d8a98166cb2c4d4c9950a6587f50
Горчаг Михаил <m.dekart@dekart.com>
2016-01-15 23:18:16 +0300

    Added  models
    Models

---
+++ SocialNetwork/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialNetwork.Models
+{
+    public class Message
+    {
+        public Guid MessageId { get; set; }
+        public string Text { get; set; }
+        public User Sender { get; set; }
+        public User Reciever { get; set; }
+        public Song SharedSong { get; set; }
+    }
+}
---
+++ SocialNetwork/Models/Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialNetwork.Models
+{
+    public class Song
+    {
+        public Guid SongId { get; set; }
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public string Lyrics { get; set; }
+        public string FilePath { get; set; }
+
+        public virtual User Publisher { get; set; }
+    }
+}

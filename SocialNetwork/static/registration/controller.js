861dcaaffb91e983dc5a7927bdfa1989fcbf3245
Горчаг Михаил <m.dekart@dekart.com>
2016-01-16 21:32:09 +0300

    add registration function

--- SocialNetwork/static/registration/controller.js
+++ SocialNetwork/static/registration/controller.js
@@ -1,4 +1,18 @@
-RegistrationModule.controller('RegistrationCtrl', function ($scope) {
-	$scope.model = 'Registraion';
-});
+RegistrationModule.controller('RegistrationCtrl',['$scope', '$http', function ($scope, $http) {
+
+	var newUserData = $scope.newUserModel = {};
+
+	$scope.sendDataForRegistration = function () {
+		$http({
+		  method: 'POST',
+		  url: '/api/Account/',
+		  data: newUserData
+		}).then(function successCallback(response) {
+		    console.log(response);
+		}, function errorCallback(response) {
+		    console.log(response);
+		});
+		console.log(newUserData);
+	};
+}]);
 

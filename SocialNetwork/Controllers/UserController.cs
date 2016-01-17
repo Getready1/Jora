8f31d1186ff1545105c7ed7e57ad943c6d1826ec
Getready1 <getready777@mail.ru>
2016-01-17 16:42:41 +0300

    UserController
    GetById

---
+++ SocialNetwork/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using SocialNetwork.Models.EntityModels;
+using SocialNetwork.Services.UserService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SocialNetwork.Controllers
+{
+    public class UserController : ApiController
+    {
+        private IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            this._userService = userService;
+        }
+ 
+        [HttpPost]
+        [Route("api/User/Login")]
+        public User GetInfoById(int id)
+        {
+            return this._userService.GetById(id);
+        }
+    }
+}
